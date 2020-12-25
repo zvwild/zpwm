99a09a5c7caa691fe057d07b2e191adb56494534
Robin Füllgraf <fuellgrafrobin@gmail.com>
2020-12-25 19:32:38 +0100

    Read in master password

--- zpwm.c
+++ zpwm.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "error_codes.h"
+
+#define MAX_PASSWORD_LENGTH 512
 
 int main(int argc, const char *argv[])
 {
@@ -13,11 +17,26 @@ int main(int argc, const char *argv[])
         const char *file_name = argv[1];
         fprintf(stderr, "Operating on file: %s\n", file_name);
 
-        if (!access(file_name, R_OK | W_OK)) {
+        if (access(file_name, R_OK | W_OK) != 0) {
                 fprintf(stderr, "Could not access input file!\n");
                 return ERROR_CANNOT_ACCESS_INPUT_FILE;
         }
 
+        fprintf(stderr, "Please enter your password: ");
+
+        const unsigned long long BUFFER_SIZE = MAX_PASSWORD_LENGTH + 1;
+        char *password = (char*)malloc(BUFFER_SIZE);
+
+        if (!password) {
+                fprintf(stderr, "Failed to allocate %llu bytes for the password!\n", BUFFER_SIZE);
+                return ERROR_CANNOT_ALLOCATE_PASSWORD_BUFFER;
+        }
+
+        scanf("%[^\n]", password);
+
+        /* clear the password buffer */
+        memset(password, 0, BUFFER_SIZE);
+        free(password);
         return 0;
 }
 
